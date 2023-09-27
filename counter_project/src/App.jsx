ab3869111197a00c96a4c2d82f0a8da34b4627a1
Ashutosh Yadav <ashutosh_yadav@Ashutoshs-MacBook-Air.local>
2023-09-27 15:46:41 +0530

    basic counter project => basic of useState

---
+++ counter_project/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+   let [counter , setCounter]= useState(0)
+
+  // let counter = 0
+
+  function addValue(){
+
+    if (counter < 20){
+      counter = counter + 1
+      setCounter(counter)
+    }
+
+
+    
+    
+  }
+
+
+  function removeValue(){
+    // console.log(Math.random())
+
+    if (counter > 0 ){
+      counter = counter - 1
+      setCounter(counter)
+    }
+    // console.log(counter)
+   
+    
+  }
+  
+
+  return (
+    <>
+     <h1>hey there we are learning react!</h1>
+     <h2>counter Value: {counter}</h2>
+     <button onClick={addValue}>Add value</button>
+     <br />
+     <button onClick={removeValue}>remove value</button>
+    </>
+  )
+}
+
+export default App
