63e410388387f15250a35e18e037255c348c1f2b
Ashutosh Yadav <ashutosh_yadav@Ashutoshs-MacBook-Air.local>
2023-09-27 14:58:19 +0530

    creating our own jsx

--- 01_vite_react/src/App.jsx
+++ 01_vite_react/src/App.jsx
@@ -2,12 +2,12 @@ import Chai from "./chai.jsx";
 
 
 function App() { 
-  
+  const userName = 'Ashutosh'
 
   return ( // using div to get more then one element or you can use empty tag also like => <></>
     <>  
     <Chai />
-    <h2>chai is ready come and get it!</h2>
+    <h2>chai is ready come and get it! {userName}</h2>
     <p>let's go</p>
     </>
 )
--- 01_vite_react/src/main.jsx
+++ 01_vite_react/src/main.jsx
@@ -12,10 +12,24 @@ function MyApp(){
   )
 }
 
+const anotherEvent = (
+  <a href="https://google.com">Visit Google</a>
+)
+
+// how to create cusom react
+
+const customReact = React.createElement(
+  'a',
+  {href: 'https://google.com' , target: '_blank'},
+  'Click me to visit'
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   
     
     // MyApp() you can write like this also but this is not a common or a good practice
-    <MyApp /> // this one is used mainly 
+    // <MyApp /> // this one is used mainly 
+    // anotherEvent
+    <App />
   
 )
